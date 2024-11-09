c9fb994015e28ef8d62029e6268f80df2ad235b9
unknown <whosinghajay@gmail.com>
2024-11-09 17:07:41 +0530

    provider for store added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigation from './src/navigators/TabNavigation';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
           <Stack.Screen
@@ -15,7 +17,7 @@ const App = () => {
             options={{animation: 'slide_from_bottom'}}></Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
-    </>
+    </Provider>
   );
 };
 
