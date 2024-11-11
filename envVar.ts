3e512ce0871a9ea7cdd0f6fe27ac9615073858b8
unknown <whosinghajay@gmail.com>
2024-11-11 18:31:59 +0530

    serverid

--- envVar.ts
+++ envVar.ts
@@ -1,2 +1,3 @@
 export const API_SERVER =
-  'https://0e49-2401-4900-1c18-31a-8856-8700-25a2-d400.ngrok-free.app';
+  'https://93f8-2401-4900-1c19-a3c-d19f-65a-c522-5850.ngrok-free.app';
+  // 'https://turf-backend-x3y6.onrender.com';
